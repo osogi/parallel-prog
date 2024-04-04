ffc65b0b344199a39a556ee1a43795e1ef1f16fe
Osogi <dragonmail135159@gmail.com>
2024-04-04 14:15:57 +0300

    task-2: copy networking project

---
+++ 2/my-networking/client.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+#define ADDRESS "127.0.0.1"
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[]) {
+	char buffer[BUFFER_SIZE] = {0};
+	std::string input;
+	
+	int sock = 0, valread = 0;
+	struct sockaddr_in serv_addr;
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+		
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		std::cout << "[ERROR] CAN'T CREATE SOCKET" << "\n";
+		return 0;
+	}
+	
+	// assign IP, PORT
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if (inet_pton(AF_INET, ADDRESS, &serv_addr.sin_addr) == -1) {
+		std::cout << "[ERROR] INVALID ADDRESS/ ADDRESS NOT SUPPORTED" << "\n";
+		return 0;
+	}
+	
+	// connect
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+		std::cout << "[ERROR] CAN'T CONNECT TO THE HOST " << ADDRESS << ":" << PORT << "\n";
+		return -1;
+	}
+	
+	for (;;) {
+		std::cout << "[TCP]> ";
+		std::cin >> input;
+		
+		if (input == "") {
+			input = "default message";
+		} else if (input == "exit") {
+			std::cout << "Client connection terminated\n";
+			break;
+		}
+		
+		if (send(sock, input.c_str(), input.size(), 0) > 0) {
+        	std::cout << "[SEND] " << input << "\n";
+        } else {
+        	std::cout << "Failed to transfer\n";
+        }
+		
+		if (recv(sock, buffer, BUFFER_SIZE, 0) > 0) {
+			std::cout << "[RECEIVE] " << buffer << "\n";
+			// clear server response data
+			memset(buffer, 0, BUFFER_SIZE);
+		} else {
+			std::cout << "[ERROR] CAN'T READ SERVER RESPONSE" << "\n";
+		}
+	}
+		
+	// close connection
+	close(sock);
+	return 0; 
+}
---
+++ 2/my-networking/server.cpp
@@ -0,0 +1,158 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ADDRESS               "0.0.0.0"
+#define PORT                  12345
+#define CONCURRENT_CONNECTION 200
+#define QUEUE_CONNECTION      100
+#define BUFFER_SIZE           65356
+#define THREAD_STACK_SIZE     524288
+
+pthread_mutex_t connection_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int connection = 0;
+
+void* connection_handler(void*);
+
+int main(int argc, char* argv[]) {
+    pthread_t thread_id;
+
+    pthread_attr_t attr;
+
+    if (pthread_attr_init(&attr) != 0) {
+        printf("[ERROR][THREAD][INIT] %s\n", strerror(errno));
+        return 1;
+    }
+
+    if (pthread_attr_setstacksize(&attr, THREAD_STACK_SIZE) != 0) {
+        printf("[ERROR][THREAD][STACK] %s\n", strerror(errno));
+        return 1;
+    }
+
+    if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) != 0) {
+        printf("[ERROR][THREAD][DETACH] %s\n", strerror(errno));
+        return 1;
+    }
+
+    int master_socket, conn_id;
+    struct sockaddr_in server, client;
+
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
+
+    signal(SIGPIPE, SIG_IGN);
+
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("[ERROR] CAN'T CREATE TO SOCKET\n");
+        return 1;
+    } else {
+        printf("[NOTE] SOCKET CREATED DONE\n");
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(ADDRESS);
+    server.sin_port = htons(PORT);
+
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+
+    if (bind(master_socket, (struct sockaddr*)&server, sizeof(server)) == -1) {
+        printf("[ERROR][BIND] %s\n", strerror(errno));
+        return 1;
+    } else {
+        printf("[NOTE] BIND %s:%d\n", ADDRESS, PORT);
+    }
+
+    if (listen(master_socket, QUEUE_CONNECTION) == -1) {
+        printf("[ERROR][LISTEN] %s\n", strerror(errno));
+        return 1;
+    } else {
+        printf("[INFO] WAITING FOR INCOMING CONNECTIONS\n");
+    }
+
+    while (1) {
+        conn_id = accept(master_socket, (struct sockaddr*)&client, (socklen_t*)&addrlen);
+
+        if (conn_id == -1) {
+            printf("[WARNING] CAN'T ACCEPT NEW CONNECTION\n");
+        } else {
+            if (connection >= CONCURRENT_CONNECTION) {
+                printf("[WARNING] CONNECTION LIMIT REACHED\n");
+                close(conn_id);
+            } else {
+                int* conn_id_heap = (int*)malloc(sizeof(int));
+
+                if (conn_id_heap == NULL) {
+                    perror("[ERROR] Memory allocation failed");
+                    close(conn_id);
+                    continue;
+                }
+
+                *conn_id_heap = conn_id;
+
+                printf("[INFO] NEW CONNECTION ACCEPTED FROM %s:%d\n", inet_ntoa(client.sin_addr),
+                       ntohs(client.sin_port));
+
+                if (pthread_create(&thread_id, &attr, connection_handler, (void*)conn_id_heap) == -1) {
+                    printf("[WARNING] CAN'T CREATE NEW THREAD\n");
+                    close(conn_id);
+                    free(conn_id_heap);
+                } else {
+                    printf("[INFO] NEW THREAD CREATED\n");
+                    connection++;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+void* connection_handler(void* sock_fd) {
+    int conn_id = *((int*)sock_fd);
+    free(sock_fd);
+
+    clock_t start, end;
+    start = clock();
+
+    char buffer[BUFFER_SIZE] = {0};
+
+    char response[1024] = "user=hello&pass=1234\n";
+
+    while (recv(conn_id, buffer, BUFFER_SIZE, 0) > 0) {
+        printf("[RECEIVED] %s\n", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+
+        if (send(conn_id, response, strlen(response), 0) > 0) {
+            printf("[SEND] %s\n", response);
+        } else {
+            printf("[WARNING][SEND] %s\n", strerror(errno));
+        }
+    }
+
+    close(conn_id);
+
+    printf("[INFO] CONNECTION CLOSED\n");
+
+    pthread_mutex_lock(&connection_mutex);
+    connection--;
+    pthread_mutex_unlock(&connection_mutex);
+
+    printf("[INFO] THREAD TERMINATED\n");
+
+    printf("[DEBUG] CONNECTION LIVE: %d\n", connection);
+
+    end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("[TIME] PROCESS COMPLETE IN %.5f SEC\n", time_taken);
+    printf("------------------------\n");
+
+    pthread_exit(NULL);
+}
